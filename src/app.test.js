79d729669a17249b7fae61e49e6dc5ade50cbd17
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-21 15:19:43 -0700

    added secrets to netlify

--- src/app.test.js
+++ src/app.test.js
@@ -1,3 +1,13 @@
-test('1=1', () => {
-  expect(1).toBe(1);
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header/Header';
+
+test('renders home link', () => {
+  render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/home/i);
+  expect(linkElement).toBeInTheDocument();
 });
