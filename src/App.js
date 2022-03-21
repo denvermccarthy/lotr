3ff2d0ec2a53ecab779f8bef9defb9ef7acd5253
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-21 11:49:40 -0700

    added header with working links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
 import Books from './views/Books/Books';
 import Characters from './views/Characters/Characters';
 import Films from './views/Films/Films';
@@ -10,6 +11,7 @@ import Main from './views/Main/Main';
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/">
           <Main />
